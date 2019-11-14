27712cafd92663f57d35504df1e4aadfee9333d5
asalimil <43479233+asalimil@users.noreply.github.com>
2019-11-14 13:22:27 -0700

    Update matrix_norm_minimization.m

--- matrix_norm_minimization.m
+++ matrix_norm_minimization.m
@@ -1,1 +1,30 @@
+%% LMI for Matrix Norm Minimization
+clc; clear; close all;
 
+t = sdpvar(1);
+
+x1 = sdpvar(1);
+x2 = sdpvar(1);
+
+A0 = [1 -4; 2 1];
+A1 = [2 -1; -1 2];
+A2 = [1 -2; 1 3];
+
+% eta = 0.00000001;
+C = [];
+A = A0 + A1*x1 + A2*x2;
+M = [-t*eye(2)  A
+      A'     -t*eye(2)];
+F = [C, M <= 0];
+optimize(F,t);
+
+% maximum eigenvalues
+t = value(t)
+
+% x_{i}
+x1 = value(x1)
+x2 = value(x2)
+
+% norm of matrix
+A = A0 + A1*x1 + A2*x2;
+norm(A)
